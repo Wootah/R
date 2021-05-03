efa6de6610905316bbba1643e8957a5d3c20033a
Wootah <matthew-kay@hotmail.com>
2021-05-03 22:43:46 +1000

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,53 @@
+var block_to_insert ;
+var container_block ;
+var backgroundtoheader;
+
+backgroundtoheader = document.createElement('div');
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+block_to_insert = document.createElement('div');
+backgroundtoheader.appendChild(block_to_insert); 
+
+backgroundtoheader.setAttribute("id", "backgroundtoheader");
+container_block = document.getElementsByClassName('main-header');
+//container_block = document.getElementsByClassName('box-header');
+
+container_block[0].appendChild(backgroundtoheader);
+
+
+//alert(1)
